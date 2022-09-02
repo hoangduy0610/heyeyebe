ed593a36dd04df868d15e9ff73427ce9310133eb
Tuan Vu Thanh <tuan.vuthanh@ncc.asia>
2022-09-02 16:16:56 +0700

    Add automatically train data

--- app/controllers/recog.controller.js
+++ app/controllers/recog.controller.js
@@ -46,14 +46,25 @@ exports.train = async (req, res, next) => {
     await faceapi.nets.faceExpressionNet.loadFromDisk('app/ML_Models/weights')
     await faceapi.nets.faceRecognitionNet.loadFromDisk('app/ML_Models/weights')
     let willBeTraining = await getDirectories('app/ML_Models/datasets');
-    const obj_trained = JSON.parse(await readPretrained());
+    let obj_trained = JSON.parse(await readPretrained());
     const trained = await obj_trained.map(e => {
-        if (e) return e.label
+        if (e) return {
+            label: e.label,
+            dataSets: e.descriptors.length
+        }
         return;
     });
-    willBeTraining = willBeTraining.filter((el) => !trained.includes(el));
+    willBeTraining = willBeTraining.filter((el) => {
+        const numOfDataSets = getFiles(`app/ML_Models/datasets/${el}`).length;
+        return !Boolean(trained.find(x => x.label == el && x.dataSets == numOfDataSets))
+    });
     const labeledFaceDescriptors = await loadLabeledImages(willBeTraining)
-    if (willBeTraining.length) await saveFile('pretrained.ds', JSON.stringify([...obj_trained, ...labeledFaceDescriptors]))
+    if (willBeTraining.length) {
+        obj_trained = obj_trained.filter(el => {
+            return !Boolean(willBeTraining.find(x => x == el.label))
+        })
+        await saveFile('pretrained.ds', JSON.stringify([...obj_trained, ...labeledFaceDescriptors]))
+    }
 
     await res.send({ message: "Successfully trained " + willBeTraining.length + " labels" })
 }
@@ -76,16 +87,23 @@ function getDirectories(path) {
     });
 }
 
+function getFiles(path) {
+    return fs.readdirSync(path).filter(function (file) {
+        return fs.statSync(path + '/' + file).isFile();
+    });
+}
+
 function loadLabeledImages(labels) {
     // const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
     return Promise.all(
         labels.map(async label => {
             const descriptions = []
-            for (let i = 1; i <= 2; i++) {
-                // const img = await faceapi.fetchImage(`labeled_images/${label}/${i}.jpg`)
-                const img = await canvas.loadImage(`app/ML_Models/datasets/${label}/${i}.jpg`)
+            const files_train = await getFiles(`app/ML_Models/datasets/${label}`);
+            for (let i = 0; i < files_train.length; i++) {
+                console.log(`app/ML_Models/datasets/${label}/${files_train[i]}`);
+                const img = await canvas.loadImage(`app/ML_Models/datasets/${label}/${files_train[i]}`);
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-                descriptions.push(detections.descriptor)
+                descriptions.push(detections?.descriptor || new Float32Array([]))
             }
 
             return new faceapi.LabeledFaceDescriptors(label, descriptions)
